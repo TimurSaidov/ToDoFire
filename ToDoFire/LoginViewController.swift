6ddce6a7a090fed1e4b7aa79dd294e92487a82a7
TimurSaidov <tim77777@inbox.ru>
2018-12-07 17:21:42 +0300

    Added Login screen, Tasks screen, Firebase

---
+++ ToDoFire/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ToDoFire
+//
+//  Created by Timur Saidov on 07/12/2018.
+//  Copyright © 2018 Timur Saidov. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var warningLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerButtonTapped(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
